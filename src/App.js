1c0d828126a6d2aa98fd86d68c46c79103878590
unknown <umutertakus@hotmail.com>
2022-02-02 23:30:03 +0300

    Create posts interface

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const [postList, setPostList] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://react-yazi-yorum.herokuapp.com/posts")
+      .then(response => {
+        setPostList(response.data);
+      })
+  }, [])
+
   return (
-    <div>
-      
+    <div className="main_wrapper">
+      <header>
+
+      </header>
+      <div className="ui raised very padded text container segment">
+        <div className="ui relaxed divided list">
+          {postList.map(post => {
+            return (<div key={post.id} className="item">
+              <i className="large github middle aligned icon"></i>
+              <div className="content">
+                <a className="header"> {post.title} </a>
+                <div className="description"> {post.created_at} </div>
+              </div>
+            </div>)
+          })}
+
+        </div>
+      </div>
     </div>
   );
 }
