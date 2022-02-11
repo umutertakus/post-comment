ecdb03e3de97a48ba9c985e84c5e72ec93958c32
unknown <umutertakus@hotmail.com>
2022-02-11 16:13:50 +0300

    Add posting form functionality

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,23 +1,52 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { withRouter } from "react-router-dom";
 
-const PostForm = () => {
+const PostForm = (props) => {
+
+    const [post, setPost] = useState({ title: "", content: "" });
+    const [errorMessage, setErrorMessage] = useState("");
+
+    const onInputChange = event => setPost({ ...post, [event.target.name]: event.target.value });
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        setErrorMessage("");
+        axios.post("https://react-yazi-yorum.herokuapp.com/posts", post)
+            .then((response) => {
+                props.history.push("/");
+            })
+            .catch(error => {
+                setErrorMessage("Başlık ve yazı içeriği zorunludur.")
+            })
+    }
+
     return (
-        <div className="ui form">
-            <div className="field">
-                <label>Yazı Başlığı</label>
-                <input type="text" />
+        <React.Fragment>
+            {errorMessage && (<div className="ui error message">
+                <div className="header">Hata</div>
+                <p>{errorMessage}</p>
+            </div>)}
+
+            <div className="ui form">
+                <div className="field">
+                    <label>Yazı Başlığı</label>
+                    <input value={post.title} type="text" name="title" onChange={onInputChange} />
+                </div>
+                <div className="field">
+                    <label>Yazı İçeriği</label>
+                    <textarea value={post.content} name="content" onChange={onInputChange} rows="3"></textarea>
+                </div>
+                <button
+                    onClick={onFormSubmit}
+                    className="ui primary button">
+                    Gönder
+                </button>
+                <button className="ui button">
+                    İptal Et
+                </button>
             </div>
-            <div className="field">
-                <label>Yazı İçeriği</label>
-                <textarea rows="3"></textarea>
-            </div>
-            <button className="ui primary button">
-                Gönder
-            </button>
-            <button className="ui button">
-                İptal Et
-            </button>
-        </div>)
+        </React.Fragment>)
 };
 
-export default PostForm;
+export default withRouter(PostForm);
